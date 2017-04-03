a721f68c9511a62b2cb7eefce383e433afefed06
Tinashe Chinyanga <Tinashe Chinyanga>
2017-04-03 08:58:40 +0200

    Initial commit

---
+++ payncollect/src/main/java/zw/co/tinashechinyanga/payncollect/App.java
@@ -0,0 +1,13 @@
+package zw.co.tinashechinyanga.payncollect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
